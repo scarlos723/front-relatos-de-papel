4eaae38ce7191e594cb1aab3d83e6d5b7e771a78
Jorge Castillo <60049228+jmcc153@users.noreply.github.com>
2025-05-07 14:38:25 -0500

    [Add] background image landing page (#39)

--- src/pages/AccessPage/index.tsx
+++ src/pages/AccessPage/index.tsx
@@ -1,28 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import backgroundImage from "../../assets/images/library.jpg";
 export const AccessPage = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
+  /* useEffect(() => {
     const timer = setTimeout(() => {
       navigate("/catalogo");
     }, 5000);
     return () => clearTimeout(timer);
-  }, []);
+  }, []); */
+
+  const handleRegisterBtn = () => {
+    navigate("/register");
+  }
+  const handleCatalogBtn = () => {
+    navigate("/catalogo");
+  };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <div className="text-center max-w-lg p-6">
-        <h1 className="text-4xl font-bold mb-4">Relatos de Papel</h1>
-        <p className="text-lg text-gray-400 mb-6">
+    <div className="relative w-full flex items-center justify-center h-screen text-white ">
+      <img
+        src={backgroundImage}
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover blur-sm brightness-75"
+      />
+      <div className="text-center max-w-lg p-6 z-10">
+        <h1 className="text-4xl text-white font-bold mb-4">Relatos de Papel</h1>
+        <p className="text-lg text-gray-100 mb-6">
           Bienvenido a Relatos de Papel, una librería donde cada historia cobra
           vida.
         </p>
         <div className="flex justify-center gap-4">
-          <button className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
+          <button onClick={handleRegisterBtn} className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition cursor-pointer">
             Registrarse
           </button>
-          <button className="px-6 py-3 bg-gray-700 text-white rounded-lg hover:bg-gray-600 transition">
+          <button onClick={handleCatalogBtn} className="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-600 transition cursor-pointer">
             Ver Catálogo
           </button>
         </div>
