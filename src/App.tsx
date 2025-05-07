253297efd0fad05210705c847a5c117c2c1a090c
scarlos723 <carlossde58@gmail.com>
2025-05-06 20:47:09 -0500

    useRoutes implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { BrowserRouter, Route } from "react-router-dom";
-import RoutesNotFound from "./components/RoutesNotFound";
-import { mainRoutes } from "./routes";
+
+import { useRoutes } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import { AccessPage } from "./pages/AccessPage";
+import { Catalog } from "./pages/Catalog";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <RoutesNotFound>
-        {mainRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.component} />
-        ))}
-      </RoutesNotFound>
-    </BrowserRouter>
-  );
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <AccessPage />,
+    },
+    { 
+      path: "/catalogo", 
+      element: <Catalog /> 
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    }
+  ]);
+  return element
 }
 
 export default App;
